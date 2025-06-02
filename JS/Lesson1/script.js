3fe56d27479c50cdaf3b8ad1fa73e2a1e93144a1
koka-patsatsia <koka.patsatsia@gmail.com>
2025-06-02 17:21:09 +0400

    homework

---
+++ JS/Lesson1/script.js
@@ -0,0 +1,31 @@
+let x = 100
+if(x<50){
+    console.log("less than 50");
+}
+    else if(x>20){
+        console.log("more than 20")
+    }
+    else{
+        console.log("error")
+    }
+
+
+
+    let userName = "Mariam";
+
+    switch (userName) {
+        case  "Mariam":
+        console.log("true");
+        break;
+        default:
+            console.log("false");
+    }
+
+
+
+    if( userName == "Mariam"){
+        console.log("true");
+    }
+    else{
+        console.log("false")
+    }
